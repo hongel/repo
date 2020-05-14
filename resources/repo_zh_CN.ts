2988c3114b99cf04480e8848ef8907519329883d
chengyulong <chengyulong@uniontech.com>
2020-05-14 17:12:49 +0800

    update translate file

---
+++ resources/repo_zh_CN.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="zh_CN" version="2.1">
+<context>
+    <name>Widget</name>
+    <message>
+        <location filename="../widget.cpp" line="11"/>
+        <source>Deepin</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="12"/>
+        <source>UnionTech is a company that develop Computer OS in China.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="13"/>
+        <source>UnionTech is the mother company of Deepin.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="14"/>
+        <source>Hello.</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+</TS>
