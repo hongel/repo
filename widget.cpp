b5071afd356f27ba8e19d6ce532a658bada9656c
chengyulong <chengyulong@uniontech.com>
2020-05-14 17:27:07 +0800

    add label3

--- widget.cpp
+++ widget.cpp
@@ -11,6 +11,8 @@ Widget::Widget(QWidget *parent)
     QLabel *label=new QLabel(tr("Deepin"),this);
     QLabel *label1=new QLabel(tr("UnionTech is a company that develop Computer OS in China."),this);
     QLabel *label2=new QLabel(tr("UnionTech is the mother company of Deepin."),this);
+    QLabel *label3=new QLabel(tr("Hello."),this);
+    QLabel *label3=new QLabel(tr("world."),this);
     layout->addWidget(label);
     layout->addWidget(label1);
 }
